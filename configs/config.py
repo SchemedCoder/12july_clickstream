adbc5d0b38c1c1f5d92a46aedba4c27f386a1560
Navin Tigga <146591653+SchemedCoder@users.noreply.github.com>
2026-07-18 00:29:26 +0530

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,19 +1,43 @@
+# ==========================================
 # Kafka Configuration
+# ==========================================
+
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
+
 CLICKSTREAM_TOPIC = "clickstream_events"
+
 EVENTS_PER_SECOND = 5
 
+
+# ==========================================
 # Bronze Layer
+# ==========================================
+
 BRONZE_PATH = "data/bronze"
+
 CHECKPOINT_BRONZE = "checkpoints/bronze"
 
+
+# ==========================================
 # Silver Layer
+# ==========================================
+
 SILVER_PATH = "data/silver"
+
 CHECKPOINT_SILVER = "checkpoints/silver"
 
+
+# ==========================================
 # Gold Layer
+# ==========================================
+
 GOLD_PATH = "data/gold"
+
 CHECKPOINT_GOLD = "checkpoints/gold"
 
+
+# ==========================================
 # Dead Letter Queue
+# ==========================================
+
 BAD_RECORDS_PATH = "data/bad_records"
